b4f8262a18eda0ec189b0dce72bb95d095762fdf
ka-few <njorovista@gmail.com>
2025-08-06 15:01:46 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  # No elements
+    elif length == 1:
+        print([0])  # Only the first number
+    elif length == 2:
+        print([0, 1])  # First two numbers
+    else:
+        fib = [0, 1]
+        while len(fib) < length:
+            fib.append(fib[-1] + fib[-2])
+        print(fib)
+
+print_fibonacci(20)
